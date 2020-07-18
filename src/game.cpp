de8efbe10463ec549851e7121ba0a2389dab399f
Hector <hectora7@gmail.com>
2020-07-18 16:02:25 +0200

    Corrected a bug that placed the food out of the window. Speed change was adjusted.

--- src/game.cpp
+++ src/game.cpp
@@ -5,19 +5,18 @@
 Game::Game(std::size_t grid_width, std::size_t grid_height)
     : snake(grid_width, grid_height),
       engine(dev()),
-      random_w(0, static_cast<int>(grid_width)),
-      random_h(0, static_cast<int>(grid_height)) {
+      random_w(0, static_cast<int>(grid_width - 1)),
+      random_h(0, static_cast<int>(grid_height - 1)) {
   PlaceFood();
 }
 
-void Game::Run(Controller const &controller, Renderer renderer,
-               std::size_t target_frame_duration) {
+void Game::Run(Controller const &controller, Renderer renderer, std::size_t const target_frame_duration) {
   Uint32 title_timestamp = SDL_GetTicks();
-  Uint32 frame_start;
-  Uint32 frame_end;
-  Uint32 frame_duration;
-  int frame_count = 0;
-  bool running = true;
+  Uint32 frame_start(0);
+  Uint32 frame_end(0);
+  Uint32 frame_duration(0);
+  int frame_count(0);
+  bool running(true);
 
   while (running) {
     frame_start = SDL_GetTicks();
@@ -60,6 +59,7 @@ void Game::PlaceFood() {
     if (!snake.SnakeCell(x, y)) {
       food.x = x;
       food.y = y;
+      //std::cout << "X: " << x << "\nY: " << y << std::endl;
       return;
     }
   }
@@ -76,10 +76,11 @@ void Game::Update() {
   // Check if there's food over here
   if (food.x == new_x && food.y == new_y) {
     score++;
+    std::cout << "Food eaten!" << std::endl;
     PlaceFood();
     // Grow snake and increase speed.
     snake.GrowBody();
-    snake.speed += 0.02;
+    snake.speed += 0.005;
   }
 }
 
